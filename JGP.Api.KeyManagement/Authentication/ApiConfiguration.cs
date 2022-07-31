33df00830887f30bbf1650e87dc9f4584afbba05
Joshua Gwynn-Palmer <josh@fanfinders.com>
2022-07-31 12:22:22 +0100

    Add project files.

---
+++ JGP.Api.KeyManagement/Authentication/ApiConfiguration.cs
@@ -0,0 +1,35 @@
+﻿// ***********************************************************************
+// Assembly         : JGP.Api.KeyManagement
+// Author           : Joshua Gwynn-Palmer
+// Created          : 07-31-2022
+//
+// Last Modified By : Joshua Gwynn-Palmer
+// Last Modified On : 07-31-2022
+// ***********************************************************************
+// <copyright file="ApiConfiguration.cs" company="JGP.Api.KeyManagement">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace JGP.Api.KeyManagement.Authentication
+{
+    using Storage;
+
+    /// <summary>
+    ///     Class ApiConfiguration.
+    /// </summary>
+    public class ApiConfiguration
+    {
+        /// <summary>
+        ///     The default scheme
+        /// </summary>
+        public const string DefaultScheme = ApiKeyConstants.DefaultScheme;
+
+        /// <summary>
+        ///     Gets or sets the services.
+        /// </summary>
+        /// <value>The services.</value>
+        public List<ServiceRecord> Services { get; set; } = new();
+    }
+}
---
+++ JGP.Api.KeyManagement/Storage/IApiKeyCacheService.cs
@@ -0,0 +1,37 @@
+﻿// ***********************************************************************
+// Assembly         : JGP.Api.KeyManagement
+// Author           : Joshua Gwynn-Palmer
+// Created          : 07-31-2022
+//
+// Last Modified By : Joshua Gwynn-Palmer
+// Last Modified On : 07-31-2022
+// ***********************************************************************
+// <copyright file="IApiKeyCacheService.cs" company="JGP.Api.KeyManagement">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+namespace JGP.Api.KeyManagement.Storage;
+
+/// <summary>
+///     Interface IApiKeyCacheService
+///     Implements the <see cref="System.IDisposable" />
+/// </summary>
+/// <seealso cref="System.IDisposable" />
+public interface IApiKeyCacheService : IDisposable
+{
+    /// <summary>
+    ///     Gets the service asynchronous.
+    /// </summary>
+    /// <param name="serviceId">The service identifier.</param>
+    /// <param name="serviceName">Name of the service.</param>
+    /// <returns>ValueTask&lt;ServiceRecord&gt;.</returns>
+    ValueTask<ServiceRecord> GetServiceAsync(Guid serviceId, string serviceName);
+
+    /// <summary>
+    ///     Gets the services asynchronous.
+    /// </summary>
+    /// <returns>ValueTask&lt;List&lt;ServiceRecord&gt;&gt;.</returns>
+    ValueTask<List<ServiceRecord>> GetServicesAsync();
+}
